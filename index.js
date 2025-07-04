70cb667ddb304f9ae003bb30e79ded16374c001a
ankitjt <48979824+ankitjt@users.noreply.github.com>
2025-07-04 15:56:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import './expenses/addExpenseButton.js'
 import './expenses/dropdowns.js'
-import './expenses/spentOnLoad.js'
+// import './expenses/spentOnLoad.js'
+import './expenses/spentOnLoad2.js'
 import './expenses/categoryLogs.js'
 
 
